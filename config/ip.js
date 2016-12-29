bf63e7ebe493f6cd0887f19733ef91c4d2e2e1e6
Rekey Luo <Rekey Luo>
2016-12-30 01:09:23 +0800

    save

---
+++ config/ip.js
@@ -0,0 +1,44 @@
+module.exports = [
+    {
+        name:'hk1',
+        aes:'aes-256-cfb',
+        port:27752,
+        password:'KTnE5N'
+    },
+    {
+        name:'hk2',
+        aes:'aes-256-cfb',
+        port:27752,
+        password:'KTnE5N'
+    },
+    {
+        name:'hk3',
+        aes: 'rc4-md5',
+        port:27752,
+        password:'KTnE5N'
+    },
+    {
+        name:'jp1',
+        aes:'aes-256-cfb',
+        port:27752,
+        password:'KTnE5N'
+    },
+    {
+        name:'jp2',
+        aes:'aes-256-cfb',
+        port:27752,
+        password:'KTnE5N'
+    },
+    {
+        name:'us1',
+        aes:'aes-256-cfb',
+        port:27752,
+        password:'KTnE5N'
+    },
+    {
+        name:'us3',
+        aes:'aes-256-cfb',
+        port:27752,
+        password:'KTnE5N'
+    }
+];
---
+++ index.js
@@ -0,0 +1,44 @@
+const dns = require('dns');
+const ejs = require('ejs');
+const pinguNoot = require('pingu-noot');
+
+const ips = require('./config/ip.js');
+
+const promise = ips.map((ip) => {
+    return new Promise((resolve) => {
+        const host = `${ip.name}.wonder.cx`;
+        pinguNoot({c: 10, timeout: 1000}, host, function (stat) {
+            ip.stat = stat;
+            resolve(ip);
+        });
+    });
+});
+
+const promise2 = ips.map((ip) => {
+    return new Promise((resolve) => {
+        const host = `${ip.name}.wonder.cx`;
+        dns.lookup(host, function (err,resp) {
+            console.log(host,resp);
+            ip.ip = resp;
+            resolve();
+        });
+    });
+});
+
+Promise.all([
+    Promise.all(promise),
+    Promise.all(promise2)
+]).then(() => {
+    const ipList = ips.sort((a,b) => {
+        a.stat.avg = a.stat.avg || Math.round(Math.random() * 1000);
+        b.stat.avg = b.stat.avg || Math.round(Math.random() * 1000);
+        return a.stat.avg - b.stat.avg;
+    });
+    ejs.renderFile('./view/config.template', {
+        ipList:ipList.slice(0,4)
+    }, function(err, str){
+        console.log(str);
+    });
+}).catch((err) => {
+    console.error(err);
+});
